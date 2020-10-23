4d1d09f412daefa9c1160011eac5449f183a5b0a
chinnkarahoi <chinnkarahoi@gmail.com>
2020-10-23 10:59:23 +0000

    add shared_code.sh

---
+++ shared_code.sh
@@ -0,0 +1,12 @@
+[ -z "$FruitShareCodes" ] && {
+FruitShareCodes=cb4246716771424bbcf95ddc538f045b@b6937910fb504ebd9af4addce592c30d@a44478b71c5c4c5697012df21a52415d@f811d0cd0b014e2bbc1074dfc7d76730
+}
+[ -z "$PetShareCodes" ] && {
+PetShareCodes=MTE1NDQ5MzYwMDAwMDAwMzcwMDk1ODM=@MTE1NDAxNzcwMDAwMDAwMzcwNDc2ODc=@MTE1NDQ5OTUwMDAwMDAwMzcwNTY5MTU=@MTE1NDUyMjEwMDAwMDAwMzcwNjQ5MTM=
+}
+[ -z "$PLANT_BEAN_SHARECODES" ] && {
+PLANT_BEAN_SHARECODES=mlrdw3aw26j3xvmulpdlkkt32g35trpagxwrbca@cshk5bfjifz74parg3cxagi54zgmsndzbl24qgq@rtsljotwy2w34w3z747iokyengkawlutzbbwy3q@7oivz2mjbmnx5jrk3p2gxuwdvrorc62cj47lsra
+}
+[ -z "$SuperMarketShareCodes" ] && {
+SuperMarketShareCodes=eU9Yae3mYKkkp23QyCEWgg@eU9YOo7gOJxgsgyQgSdL@eU9YDZ_2I518sBuxnjt0@eU9YN732JrpGiBWjnyFP
+}
