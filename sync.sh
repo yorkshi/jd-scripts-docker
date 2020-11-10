672f4a9867ce84d2f8c77e0dc33e6d5daf1db7e1
chinnkarahoi <chinnkarahoi@gmail.com>
2020-11-10 05:29:54 +0000

    add crontab.list

--- sync.sh
+++ sync.sh
@@ -13,29 +13,7 @@ git clone --depth=1 https://github.com/lxk0301/jd_scripts.git /scripts_tmp
 cd /scripts || exit 1
 npm install || npm install --registry=https://registry.npm.taobao.org || exit 1
 cp /crontab.list /crontab.list.old
-echo "55 */3 * * * bash /sync 1>/proc/1/fd/1 2>/proc/1/fd/2" > /crontab.list
-for file in $(find /scripts/.github/workflows -type f);do
-  cat $file | grep -q 'cron:' && {
-    cat $file | grep -q 'node jd_.*\.js' && {
-      a=$(cat $file | sed -En "s/^.*cron: '(.*)'.*$/\1/p")
-      b=$(cat $file | sed -En 's|^.*node (jd_.*\.js)$|node /scripts/\1|p')
-      name=$(cat $file | sed -En "s|^.*name: .运行(.*)|\1|p" | sed "s/'//g")
-      echo $file
-      script="\"
-        exec 1<>/proc/1/fd/1;
-        exec 2<>/proc/1/fd/2;
-        set -o allexport;
-        source /all;
-        source /env;
-        source /jd-scripts-docker/resolve.sh;
-        cd /scripts;
-        $b | sed 's/^/$name/';
-      \""
-      script="$(echo ${script})"
-      echo "$a" bash -c "$script" >> /crontab.list
-    }
-  }
-done
+cp /jd-scripts-docker/crontab.list /crontab.list
 crontab -r
 crontab /crontab.list || {
   cp /crontab.list.old /crontab.list
