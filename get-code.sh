63e5ee93fc9366c255c2f2413796cd49e0e14d95
chinnkarahoi <chinnkarahoi@gmail.com>
2020-11-13 11:20:09 +0000

    update

--- get-code.sh
+++ get-code.sh
@@ -1,14 +1,19 @@
-CMD="docker-compose"
 code1=
 code2=
 code3=
 code4=
-srv=${srv:-jd1}
-line="$(docker logs $srv | grep -A1 '互助码' | grep -v '^--$' | sed -r "s/[[:cntrl:]]\[[0-9]{1,3}m//g" | sed 'N;s/\n/ /' | sort -u )"
-code1=$(echo "$line" | grep "东东农场" | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
-code2=$(echo "$line" | grep "东东萌宠" | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
-code3=$(echo "$line" | grep "种豆得豆" | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
-code4=$(echo "$line" | grep "京小超" | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
+for srv in $(docker-compose config --services);do
+  CMD="docker exec $srv bash -c 'set -o allexport; source /all; source /env; source /jd-scripts-docker/resolve.sh; cd /scripts; ls jd_*.js | xargs -i node {}'"
+  line="$($CMD | grep -A1 '互助码' | grep -v '^--$' | sed -r "s/[[:cntrl:]]\[[0-9]{1,3}m//g" | sed 'N;s/\n/ /' | sort -u )"
+  [ -z "$code1" ] || code1+=@
+  [ -z "$code2" ] || code2+=@
+  [ -z "$code3" ] || code3+=@
+  [ -z "$code4" ] || code4+=@
+  code1+=$(echo "$line" | grep "东东农场" | head -n 1 | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
+  code2+=$(echo "$line" | grep "东东萌宠" | head -n 1 | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
+  code3+=$(echo "$line" | grep "种豆得豆" | head -n 1 | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
+  code4+=$(echo "$line" | grep "京小超" | head -n 1 | grep -Eo '[a-zA-Z0-9_=+/]{15,}')
+done
 echo FRUITSHARECODES=$code1
 echo PETSHARECODES=$code2
 echo PLANT_BEAN_SHARECODES=$code3
