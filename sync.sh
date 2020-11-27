44b875007637d0cb31388a6f9df39a57dde678ad
chinnkarahoi <chinnkarahoi@gmail.com>
2020-11-27 08:29:44 +0000

    fix branch

--- sync.sh
+++ sync.sh
@@ -5,13 +5,12 @@ git clone --depth=1 https://github.com/chinnkarahoi/jd-scripts-docker.git /jd-sc
   rm -rf /jd-scripts-docker
   mv /jd-scripts-docker_tmp /jd-scripts-docker
 }
-git clone --depth=1 https://github.com/lxk0301/jd_scripts.git /scripts_tmp
+git clone --branch=master --depth=1 https://github.com/lxk0301/jd_scripts.git /scripts_tmp
 [ -d /scripts_tmp ] && {
   rm -rf /scripts
   mv /scripts_tmp /scripts
 }
 cd /scripts || exit 1
-
 npm install || npm install --registry=https://registry.npm.taobao.org || exit 1
 cp /crontab.list /crontab.list.old
 cp /jd-scripts-docker/crontab.list /crontab.list
